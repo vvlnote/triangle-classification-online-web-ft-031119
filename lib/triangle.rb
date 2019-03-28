f16d0910acc7801483efc97c759b177e7ed216ef
Joy Lin <vvlnote@hotmail.com>
2019-03-28 22:46:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,9 +2,9 @@ class Triangle
   # write code here
   
   def initialize(side_1, side_2, side_3)
-    @side_1 = side_1
-    @side_2 = side_2
-    @side_3 = side_3
+    @side1 = side_1
+    @side2= side_2
+    @side3 = side_3
   end
   
   def all_sides_are_vald?
@@ -12,7 +12,7 @@ class Triangle
   end
   
   def two_sides_exceeds_third_side?
-    
+    if @side
   end
   def kind
     all_sides_are_valid?
