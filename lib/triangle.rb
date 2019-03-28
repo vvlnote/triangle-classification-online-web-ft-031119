8e34573c43489be2d0aa42035def56c636e556bf
Joy Lin <vvlnote@hotmail.com>
2019-03-28 22:24:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,9 +2,12 @@ class Triangle
   # write code here
   
   def initialize(side_1, side_2, side_3)
-    
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
   end
   
+  def type
   class TriangleError < StandardError
     
   end
